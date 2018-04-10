889e9470a16789a230e82aff94b7cff9a5421212
Marcin Andrzejczak <marcin_andrzejczak@outlook.com>
2018-04-10 22:58:17 +0200

    Clean up

---
+++ src/my/labproject/logger/Logger.java
@@ -0,0 +1,13 @@
+package my.labproject.logger;
+
+public class Logger {
+
+    public void INFO(String message){
+        System.out.println("[INFO] "+message);
+    }
+
+    public void ERROR(String message){
+        System.out.println("[ERROR] "+message);
+    }
+
+}
